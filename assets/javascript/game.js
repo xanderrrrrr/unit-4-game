24fa64d0bf1340761ca05dc7a49eb35d36ff88c8
Xander C <xander.canedo@gmail.com>
2019-09-11 20:04:02 -0600

    setting randomized number to array for the 4 crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,33 @@
 $(document).ready(function() {
 
     // everything starts when the DOM is ready
-    // maybe I initialize with init function?
 
 
-    // setting my global variables
+    // setting some default variables
     var wins = 0;
-    var losses = 0 ;
-    var targetNumber = targetNumber();
-    var userNumber = 0;
+    var losses = 0;
+    var targetNumber;
+    var numberOptions;
+    var crystal1;
+    var crystal2;
+    var crystal3;
+    var crystal4;
 
-    // this is my crystal array
-    crystalArray = ["assets/images/orange-crystal.png","assets/images/green-crystal"]
-
-    // this is to generate the target number
-    function targetNumber() {
-        nmbr = Math.floor(Math.random() * 121) + 19
-        console.log(nmbr);
-        return nmbr;
-        
+    function randomizer() {
+        // this will get the adequate target number based on hw requirements
+        targetNumber = Math.floor((Math.random() * 101) + 19);
+        // this will set a random number to my array 
+        numberOptions = Array.from({ length: 4 }, () => Math.floor(Math.random() * 12) + 1);
+        // assigning my array indeces to my crystals
+        crystal1 = numberOptions[0];
+        crystal2 = numberOptions[1];
+        crystal3 = numberOptions[2];
+        crystal4 = numberOptions[3];
+        console.log(crystal1 + " " + crystal2 + " " + crystal3 + " " + crystal4)
     }
+
+    randomizer();
+
+
 
 });
